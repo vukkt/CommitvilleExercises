fe64ef3d49cf65f420b45c7e71e5bf005ff9da8a
vukkt <63306506+vukkt@users.noreply.github.com>
2021-12-26 14:03:12 +0100

    Update Ex2.java

--- src/ex2/Ex2.java
+++ src/ex2/Ex2.java
@@ -1,10 +1,26 @@
 package ex2;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Please insert first number:");
+
+		int firstNumber = scanner.nextInt();
+
+		System.out.println("Please insert second number:");
+
+		int secondNumber = scanner.nextInt();
+
+		System.out.println("Calculating sum of numbers...");
+
+		int result = firstNumber + secondNumber;
+
+		System.out.println("The sum is " + result);
 	}
 
 }
