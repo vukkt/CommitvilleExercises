fe6c082ae05de3ca90c37bf8d480c1fc5ebefd98
vukkt <63306506+vukkt@users.noreply.github.com>
2021-12-26 14:45:54 +0100

    Exercises

--- src/ex8/Ex8.java
+++ src/ex8/Ex8.java
@@ -3,7 +3,10 @@ package ex8;
 public class Ex8 {
 
 	public static void main(String[] args) {
-
+		System.out.println("   J   a  v     v  a");
+		System.out.println("   J  a a  v   v  a a");
+		System.out.println("J  J aaaaa  V V  aaaaa");
+		System.out.println(" JJ a     a  V  a     a");
 	}
 
 }
---
+++ src/ex9/Ex9.java
@@ -0,0 +1,16 @@
+package ex9;
+
+public class Ex9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 25.5;
+		double b = 3.5;
+		double c = 4.5;
+		double d = c * 9;
+
+		System.out.println((a * b - b * b) / (d - c));
+
+	}
+
+}
