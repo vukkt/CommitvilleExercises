5abfd0b2ce80554f02194fa6bf7aeeb7a0bf7ccd
vukkt <63306506+vukkt@users.noreply.github.com>
2021-12-26 14:14:29 +0100

    Update Ex4.java

--- src/ex4/Ex4.java
+++ src/ex4/Ex4.java
@@ -4,7 +4,8 @@ public class Ex4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(
+				(-5 + 8 * 6) + "\n" + ((55 + 9) % 9) + "\n" + (20 + -3 * 5 / 8) + "\n" + (5 + 15 / 3 * 2 - 8 % 3));
 	}
 
 }
