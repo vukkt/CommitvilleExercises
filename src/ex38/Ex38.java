4f1c60958601a159fb9f2665c169214c0df3c1b0
vukkt <63306506+vukkt@users.noreply.github.com>
2021-12-27 18:21:11 +0100

    Update Ex38.java

--- src/ex38/Ex38.java
+++ src/ex38/Ex38.java
@@ -1,10 +1,38 @@
 package ex38;
 
+import java.util.Scanner;
+
 public class Ex38 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please insert string: ");
+		String test = scanner.nextLine();
+		count(test);
 
 	}
 
+	public static void count(String x) {
+		char[] ch = x.toCharArray();
+		int letter = 0;
+		int space = 0;
+		int num = 0;
+		int other = 0;
+		for (int i = 0; i < x.length(); i++) {
+			if (Character.isLetter(ch[i])) {
+				letter++;
+			} else if (Character.isDigit(ch[i])) {
+				num++;
+			} else if (Character.isSpaceChar(ch[i])) {
+				space++;
+			} else {
+				other++;
+			}
+		}
+		System.out.println("letter: " + letter);
+		System.out.println("space: " + space);
+		System.out.println("number: " + num);
+		System.out.println("other: " + other);
+	}
 }
