af39e2a88c5e6ee8d51aa3ea2519a113b7aba800
vukkt <63306506+vukkt@users.noreply.github.com>
2021-12-28 16:50:19 +0100

    Exercises

---
+++ src/ex42/Ex42.java
@@ -0,0 +1,18 @@
+package ex42;
+
+import java.util.Scanner;
+
+public class Ex42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Input your Pasword: ");
+
+		String password = scanner.nextLine();
+
+		System.out.println("Your password was: " + password);
+	}
+
+}
---
+++ src/ex43/Ex43.java
@@ -0,0 +1,21 @@
+package ex43;
+
+public class Ex43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 3; i++) {
+			if (i == 0 || i == 2) {
+				System.out.println("Twinkle, twinkle, little star,");
+				System.out.println("\tHow I wonder what you are!");
+			}
+			if (i == 1) {
+				System.out.println("\t\tUp above the world so high,");
+				System.out.println("\t\tLike a diamond in the sky.");
+
+			}
+
+		}
+	}
+
+}
