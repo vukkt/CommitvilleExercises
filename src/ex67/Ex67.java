91d89fe01744259a7dd384bde1b803cde1f4a7e5
vukkt <63306506+vukkt@users.noreply.github.com>
2021-12-30 21:09:53 +0100

    Create Ex67.java

---
+++ src/ex67/Ex67.java
@@ -0,0 +1,11 @@
+package ex67;
+
+public class Ex67 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String main_string = "Python 3.0";
+		String word = "Tutorial";
+		System.out.println(main_string.substring(0, 7) + word + main_string.substring(6));
+	}
+}
