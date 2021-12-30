b2a876084d865ab4f3232133ae915f07fadcfa00
vukkt <63306506+vukkt@users.noreply.github.com>
2021-12-30 19:05:05 +0100

    Create Ex59.java

---
+++ src/ex59/Ex59.java
@@ -0,0 +1,20 @@
+package ex59;
+
+import java.util.Scanner;
+
+public class Ex59 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Input a String: ");
+
+		String inputline = scanner.nextLine();
+
+		inputline = inputline.toLowerCase();
+
+		System.out.println(inputline);
+	}
+
+}
