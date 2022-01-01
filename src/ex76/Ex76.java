dabd6300213514322b760f9780e99b33deae3d5b
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-01 20:11:33 +0100

    Exercises

--- src/ex76/Ex76.java
+++ src/ex76/Ex76.java
@@ -1,17 +1,18 @@
-package ex75;
+package ex76;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Ex75 {
+public class Ex76 {
 
 	public static void main(String[] args) {
 
-		ArrayList<Integer> array = new ArrayList<Integer>();
-
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("please enter 5 elements of the array");
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		ArrayList<Integer> array2 = new ArrayList<Integer>();
+
+		System.out.println("please enter 5 elements of the first array");
 
 		int first = scanner.nextInt();
 		int second = scanner.nextInt();
@@ -24,6 +25,21 @@ public class Ex75 {
 		array.add(third);
 		array.add(fourth);
 		array.add(fifth);
+
+		System.out.println("please enter 5 elements of the second array");
+
+		int first2 = scanner.nextInt();
+		int second2 = scanner.nextInt();
+		int third2 = scanner.nextInt();
+		int fourth2 = scanner.nextInt();
+		int fifth2 = scanner.nextInt();
+
+		array.add(first2);
+		array.add(second2);
+		array.add(third2);
+		array.add(fourth2);
+		array.add(fifth2);
+
 		System.out.println("Succesfully created an array of 5 elements :" + array);
 		if (Integer.valueOf(first).equals(fifth)) {
 			System.out.println("true");
