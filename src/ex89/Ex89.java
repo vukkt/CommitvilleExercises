43b6687279822a3dd660ac0a5d52e1d14e4d99fa
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-02 18:27:32 +0100

    Create Ex89.java

---
+++ src/ex89/Ex89.java
@@ -0,0 +1,10 @@
+package ex89;
+
+public class Ex89 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("System security interface:");
+		System.out.println(System.getSecurityManager());
+	}
+}
