5029550172d35b4baed59eb89bd07152e68eec6f
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-02 18:26:24 +0100

    Create Ex88.java

---
+++ src/ex88/Ex88.java
@@ -0,0 +1,12 @@
+package ex88;
+
+public class Ex88 {
+
+	public static void main(String[] args) {
+
+		System.out.println("\nCurrent system environment:");
+		System.out.println(System.getenv());
+		System.out.println("\n\nCurrent system properties:");
+		System.out.println(System.getProperties());
+	}
+}
