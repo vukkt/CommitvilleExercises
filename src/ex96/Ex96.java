7f0561b43076207adf0f277e9dae51362e025487
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-03 10:59:22 +0100

    Exercises

--- src/ex96/Ex96.java
+++ src/ex96/Ex96.java
@@ -3,7 +3,7 @@ package ex96;
 public class Ex96 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		int[] array_nums = { 10, 70, 80, 50, 20, 13, 50 };
 		boolean testd = false;
 		int result = 0;
