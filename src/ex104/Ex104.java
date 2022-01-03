b939d8d5dffbfa2ab44176dc19764fd6de33c08d
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-03 12:08:10 +0100

    Create Ex104.java

---
+++ src/ex104/Ex104.java
@@ -0,0 +1,23 @@
+package ex104;
+
+import java.util.Arrays;
+
+public class Ex104 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array_nums = { 11, 15, 13, 10, 45, 20, 33, 53 };
+		int result = 0;
+		System.out.println("Original Array: " + Arrays.toString(array_nums));
+
+		int l = 0;
+		int[] new_array;
+		while (array_nums[l] != 10)
+			l++;
+		new_array = new int[l];
+		for (int i = 0; i < l; i++)
+			new_array[i] = array_nums[i];
+		System.out.println("New Array: " + Arrays.toString(new_array));
+	}
+
+}
