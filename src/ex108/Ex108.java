73ad6c79d383883317dc582751bd02d5d0513950
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-04 21:54:40 +0100

    Create Ex108.java

---
+++ src/ex108/Ex108.java
@@ -0,0 +1,20 @@
+package ex108;
+
+import java.util.Scanner;
+
+public class Ex108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Input a positive integer: ");
+
+		int n = scanner.nextInt();
+
+		if (n > 0) {
+			System.out.print("The single digit number is: " + (n == 0 ? 0 : (n % 9 == 0 ? 9 : n % 9)));
+		}
+
+	}
+}
