fdb4e0b828059b8112a0029fcd4fc2340488cd27
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-06 20:27:21 +0100

    Create Ex112.java

---
+++ src/ex112/Ex112.java
@@ -0,0 +1,25 @@
+package ex112;
+
+import java.util.Scanner;
+
+public class Ex112 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input a number: ");
+
+		int n = scanner.nextInt();
+
+		int n1 = n;
+		long ctr = 0;
+		while (n != 0) {
+
+			ctr += n / 5;
+			n /= 5;
+		}
+		System.out.printf("Number of trailing zeros of the factorial %d is %d ", n1, ctr);
+
+	}
+}
