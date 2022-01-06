9716c77b1f95c6e525507db64984a869dae9d08f
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-06 20:24:25 +0100

    Create Ex111.java

---
+++ src/ex111/Ex111.java
@@ -0,0 +1,24 @@
+package ex111;
+
+import java.util.Scanner;
+
+public class Ex111 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please insert a integer:");
+
+		int firstInteger = scanner.nextInt();
+
+		System.out.println("Please insert a second integer: ");
+
+		int secondInteger = scanner.nextInt();
+
+		System.out.println("result  of sum is " + firstInteger + secondInteger);
+
+	}
+
+}
