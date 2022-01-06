fa04def8d017a725b2e5c54c383bad5cd7f2d771
vukkt <63306506+vukkt@users.noreply.github.com>
2022-01-06 20:45:07 +0100

    Update ex116.java

--- src/ex116/ex116.java
+++ src/ex116/ex116.java
@@ -5,6 +5,15 @@ public class ex116 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.printf("\n%d: fizz buzz", i);
+			} else if (i % 5 == 0) {
+				System.out.printf("\n%d: buzz", i);
+			} else if (i % 3 == 0) {
+				System.out.printf("\n%d: fizz", i);
+			}
+		}
+		System.out.printf("\n");
 	}
-
 }
